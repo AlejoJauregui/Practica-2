89e855b7ba9043fa2c2d1ba3d6afe4e55f721001
Alejo_Jauregui <alejojs9409@gmail.com>
2020-02-23 11:59:17 -0500

    Camera Behaviour
    We create the camera behaviour script and modify somethings in the scene

---
+++ Assets/Scripts/BulletBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehavior : MonoBehaviour
+{
+    public int velocity;
+    public GameObject objectToDestroy;
+
+    Vector3 bulletDirection;
+    PlayerBehaviour player; 
+    void FixedUpdate()
+    {
+        setDirection(transform.position);
+    }
+
+    private void OnTriggerEnter(Collider destroyTrigger)
+    {
+        if(destroyTrigger.gameObject.tag.Equals("Turret"))
+        {
+            Debug.Log("Has destruido una " + destroyTrigger.name);
+            destroyTrigger.gameObject.SetActive(false);
+        }
+        if(destroyTrigger.gameObject.tag.Equals("Player"))
+        {
+            player.Hit(objectToDestroy);
+            destroyTrigger.gameObject.SetActive(false);
+        }
+            
+    }
+
+    public void setDirection (Vector3 direction)
+    {
+        bulletDirection = direction;
+    }
+}
---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraBehaviour : MonoBehaviour
+{
+    public GameObject objectivePlayer;
+    public Vector3 targetPosition;
+    public float cameraSpeed; 
+    public float minimumHeight;
+
+    Vector3 height; 
+
+    void LateUpdate()
+    {
+        FollowPlayer();
+    }
+
+    void FollowPlayer()
+    {
+        targetPosition = new Vector3(objectivePlayer.transform.position.x, objectivePlayer.transform.position.y + minimumHeight, objectivePlayer.transform.position.z);
+        this.transform.position = Vector3.Lerp(this.transform.position, targetPosition, cameraSpeed * Time.deltaTime);  
+    }
+}
